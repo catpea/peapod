cf2db4c34cc8cc094a34bc3a94a2884a754e6de3
catpea <corporate.miou@gmail.com>
2021-07-30 17:42:06 -0400

    Always Quote echo;

--- setup/music.sh
+++ setup/music.sh
@@ -3,7 +3,7 @@
 if test -d  /srv/music; then
     echo "Already configured /srv/music";
   else
-    echo Setup Shared Music Directory (/srv/music)
+    echo "Setup Shared Music Directory (/srv/music)";
     mkdir -p /srv/music
     chgrp -R music /srv/music
     chmod -R 2775 /srv/music

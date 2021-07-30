bfe449e4cbfb8bed8094e068a896e654f29b9cfd
catpea <corporate.miou@gmail.com>
2021-07-30 17:39:01 -0400

    Run As Root Checks

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 {
 
   configure_group() {
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 gpasswd -d peapod music
 userdel peapod;
 rm -fr /home/peapod;

30076f98af0f7f8e88ed398f885cdba8087944e3
catpea <corporate.miou@gmail.com>
2021-08-01 12:20:56 -0400

    The systemd Daemon Installer

--- setup/daemon.sh
+++ setup/daemon.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 echo daemon setup...
 
-# daemon
-# read -d '' daemon <<_EOF_
-# [Unit]
-# Description=iSir Daemon
-#
-# [Service]
-# ExecStart=/usr/bin/isir /dev/input/event1
-# Restart=on-failure
-#
-# [Install]
-# WantedBy=multi-user.target
-# _EOF_
-#
-# echo $daemon > /etc/systemd/system/isir.service
+read -d '' daemon <<_EOF_
+[Unit]
+Description=iSir Daemon
+
+[Service]
+ExecStart=/usr/bin/isir /dev/input/event1
+Restart=on-failure
+StandardOutput=syslog
+User=peapod
+Group=peapod
+
+[Install]
+WantedBy=multi-user.target
+_EOF_
+
+echo $daemon > /etc/systemd/system/isir-catpea.service
+
+
+if systemctl --all --type service | grep -q "isir-catpea"; then
+  systemctl --system daemon-reload
+  systemctl enable isir-catpea
+else
+  systemctl restart isir-catpea
+fi;
+
+systemctl status isir-catpea

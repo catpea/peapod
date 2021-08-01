7737fe733963fc06b22adfd8f041115a5a97c22b
catpea <corporate.miou@gmail.com>
2021-08-01 13:22:16 -0400

    Add pnpm

--- setup/node.sh
+++ setup/node.sh
@@ -14,6 +14,8 @@ cp -R node-v14.13.0-linux-armv6l/* /usr/local
 cd -;
 rm -rf $TMP_DIR;
 
+curl -f https://get.pnpm.io/v6.7.js | node - add --global pnpm
+
 # Use these locations if something is broken.
 # https://unofficial-builds.nodejs.org/download/release/v14.17.4/node-v14.17.4-linux-armv6l.tar.xz
 # https://unofficial-builds.nodejs.org/download/release/v15.14.0/node-v15.14.0-linux-armv6l.tar.xz
